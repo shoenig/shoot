1026bf17998d32ec43096087abdd96b8af7bbd10
Seth Hoenig <shoenig@hashicorp.com>
2020-01-09 17:42:14 -0600

    tweak ffmpeg options to be more better

--- encode.sh
+++ encode.sh
@@ -4,10 +4,19 @@
 # This will modify the images in-place, so make sure these are a copy, or you
 # do not care about them.
 #
-# set OUTPUT filename
-
-# todo: maybe set size with -s ?
+# set OUTPUT filename before running
 
 set -euo pipefail
 
-ffmpeg -threads 4 -y -pattern_type glob -i '*.jpg' -target "film-dvd" -r 24 "${OUTPUT}"
+# cleanup old file
+rm -f "${OUTPUT}"
+
+ffmpeg \
+	-threads 4 \
+	-y \
+	-pattern_type glob \
+	-i '*.jpg' \
+	-framerate 24 \
+	-c:v libx264 \
+	-s 2650x1600 \
+	"${OUTPUT}"
